c9b9245dfdd661875184d1f722f587cb352e0042
koukyo1994 <arabiannight1994@yahoo.co.jp>
2018-03-20 14:24:37 +0900

    Mistook and missed the chance to submit...

--- scripts/submission_by_Ridge.py
+++ scripts/submission_by_Ridge.py
@@ -9,6 +9,7 @@ import lightgbm as lgbm
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import Ridge
 from wordbatch.models import FM_FTRL
+import gc
 
 train_path = "../datas/train.csv"
 test_path = "../datas/test.csv"
@@ -20,25 +21,6 @@ df_test = pd.read_csv(test_path)
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-
-def feature_modify(df):
-    comment_text = df['comment_text']
-    df['Length_of_comments'] = comment_text.apply(lambda x: len(x))
-    df['n_capitals'] = comment_text.apply(lambda x: len(re.findall(r"[A-Z]", x)))
-    df['prop_capitals'] = df['n_capitals'] / df['Length_of_comments']
-    df['n_exclamation'] = comment_text.apply(lambda x: len(re.findall(r"!", x)))
-    df['n_question'] = comment_text.apply(lambda x: len(re.findall(r"\?", x)))
-    df['n_punct'] = comment_text.apply(lambda x: len(re.findall(r"[#$%&*]", x)))
-    df['n_words'] = comment_text.apply(lambda x: len(x.split()))
-    df['n_unique'] = comment_text.apply(lambda x: len(np.unique(x.split())))
-    df['prop_unique'] = df['n_unique'] / df['n_words']
-    df['n_smiles'] = comment_text.apply(lambda x: len(re.findall(r":\)", x)))
-    return df
-
-new_df = feature_modify(df_train)
-new_test = feature_modify(df_test)
-print("Modifying the features.")
-
 COMMENT = 'comment_text'
 df_train[COMMENT].fillna("unknown", inplace=True)
 df_test[COMMENT].fillna("unknown", inplace=True)
@@ -48,7 +30,7 @@ re_tok = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤�
 def tokenize(s): return re_tok.sub(r' \1 ', s).split()
 
 print("Tfidf Vectorizing words.")
-n = new_df.shape[0]
+n = df_train.shape[0]
 tv = TfidfVectorizer(ngram_range=(1, 3),
                      tokenizer=tokenize,
                      min_df=3,
@@ -58,18 +40,9 @@ tv = TfidfVectorizer(ngram_range=(1, 3),
                      smooth_idf=1,
                      sublinear_tf=1,
                      stop_words="english",
-                     max_features=100000)
-trn_term_doc = tv.fit_transform(new_df[COMMENT])
-test_term_doc = tv.transform(new_test[COMMENT])
-
-print("Concatenating the feature matrices.")
-added_train = new_df.loc[:, ['n_capitals', 'prop_capitals', 'n_exclamation', 'n_unique', 'prop_unique']]
-added_test = new_test.loc[:, ['n_capitals', 'prop_capitals', 'n_exclamation', 'n_unique', 'prop_unique']]
-added_train = csr_matrix(added_train)
-added_test = csr_matrix(added_test)
-
-x = hstack([trn_term_doc, added_train])
-test_x = hstack([test_term_doc, added_test])
+                     max_features=40000)
+trn_term_doc = tv.fit_transform(df_train[COMMENT])
+test_term_doc = tv.transform(df_test[COMMENT])
 
 print("Tfidf Vectorizing characters.")
 tv = TfidfVectorizer(
@@ -78,17 +51,21 @@ tv = TfidfVectorizer(
     analyzer='char',
     stop_words='english',
     ngram_range=(2, 6),
-    max_features=100000)
-trn_char_doc = tv.fit_transform(new_df[COMMENT])
-test_char_doc = tv.transform(new_test[COMMENT])
+    max_features=40000)
+trn_char_doc = tv.fit_transform(df_train[COMMENT])
+test_char_doc = tv.transform(df_test[COMMENT])
+
+del tv
+gc.collect()
 
 print("Concatenating the feature matrices.")
-x = hstack([trn_char_doc, x])
-test_x = hstack([test_char_doc, test_x])
+x = hstack([trn_char_doc, trn_term_doc])
+test_x = hstack([test_char_doc, test_term_doc])
 
 x = x.tocsr()
 test_x = test_x.tocsr()
 
+print("Splitting the dataset.")
 x, x_eval, y, y_eval = train_test_split(x,
                                         df_train.loc[:, ['toxic', 'severe_toxic', 'obscene',
                                                          'threat', 'insult', 'identity_hate']],
@@ -97,6 +74,7 @@ x, x_eval, y, y_eval = train_test_split(x,
 
 columns = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 
+print("Started the fitting and prediction.")
 preds_Ridge = []
 for i, j in enumerate(columns):
     print('fit ', j)
@@ -110,4 +88,3 @@ for i, name in enumerate(columns):
     submission[name] = preds_Ridge[i]
 
 submission.to_csv("../submission/Ridge.csv", index=False)
-
